9c1e0dfe4a1b46a14346301941e8d3f27f1814e7
132905 <125987008+132905@users.noreply.github.com>
2024-02-26 23:52:46 +0530

    Added NativeQuery to UserRepository.java

--- src/main/java/com/harsh/learningportalnew/repository/UserRepository.java
+++ src/main/java/com/harsh/learningportalnew/repository/UserRepository.java
@@ -6,4 +6,14 @@ import com.harsh.learningportalnew.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 	UserEntity findByusername(String name);
+
+	@Query(value = "SELECT u.username AS username, c.title AS title " + "FROM users u "
+			+ "INNER JOIN registered_courses r ON u.user_Id = r.user_Id "
+			+ "INNER JOIN course c ON r.course_id = c.course_id", nativeQuery = true)
+	List<Object[]> getUsersWithRegisteredCoursesQuery();
+
+	// @Query(value = "SELECT u.username AS username, c.title AS title " + "FROM users u "
+	// 		+ "INNER JOIN favourite_Course f ON u.user_Id = f.user_Id "
+	// 		+ "INNER JOIN course c ON f.course_id = c.course_id", nativeQuery = true)
+	// List<Object[]> getUsersWithFavouriteCoursesQuery();
 }
